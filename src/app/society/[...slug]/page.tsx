ebadadaef3822082b2d4c0265920f78dfcbd526f
Irawo <timothyaremu63@gmail.com>
2025-01-10 02:17:14 +0100

    added society page

---
+++ src/app/society/[...slug]/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import SocietyLayout from "../../../features/pages/society/layout/SocietyLayout"
+
+const page = ({params}) => {
+  return (
+    <main>
+      <SocietyLayout />
+    </main>
+  )
+}
+
+export default page
