1a37e0746aac24ac21dc7213ad8ee0091a8464df
Irawo <timothyaremu63@gmail.com>
2025-01-12 15:44:49 +0100

    updated the ui

---
+++ src/components/ui/mobileHeader/MobileHeader.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import styles from "./mobileHeader.module.css"
+import Image from 'next/image'
+
+const MobileHeader = ({setShowPostArea}) => {
+  return (
+    <div className={styles.mobileHeaderContainer}>
+      <button
+        onClick={() => setShowPostArea(false)}
+      >
+        <svg aria-hidden="true" width="28" height="15" viewBox="0 0 28 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M0.292891 6.79289C-0.0976333 7.18342 -0.0976334 7.81658 0.29289 8.2071L6.65685 14.5711C7.04738 14.9616 7.68054 14.9616 8.07107 14.5711C8.46159 14.1805 8.46159 13.5474 8.07107 13.1569L2.41421 7.5L8.07107 1.84314C8.46159 1.45262 8.46159 0.819455 8.07107 0.42893C7.68054 0.038406 7.04738 0.038406 6.65685 0.42893L0.292891 6.79289ZM28 6.5L0.999998 6.5L0.999998 8.5L28 8.5L28 6.5Z" fill="#263238" fillOpacity="0.6"/>
+        </svg>
+      </button>
+
+      <Image
+        src="/ventmoir.svg"
+        alt="Ventmoir logo"
+        width={53}
+        height={52}
+        priority
+      />
+    </div>
+  )
+}
+
+export default MobileHeader
--- src/features/pages/home/layout/HomeLayout.jsx
+++ src/features/pages/home/layout/HomeLayout.jsx
@@ -1,15 +1,23 @@
-import React from 'react'
+"use client"
+import React, { useState } from 'react'
 import styles from "./homeLayout.module.css"
 import PostStream from '../sections/postStream/PostStream'
+import PostButton from "../../../../components/ui/postButton/PostButton"
+import PostArea from "../../../../components/postArea/PostArea"
 
 const HomeLayout = () => {
+  const [showPostArea, setShowPostArea] = useState(false) 
   return (
     <div className={styles.homeLayoutContainer}>
       <PostStream />
 
       <div className={styles.homeWidget}>
         <h1>Widget</h1>
+        <PostButton />
       </div>
+
+      <PostButton setShowPostArea={setShowPostArea} />
+      {showPostArea && <PostArea setShowPostArea={setShowPostArea} />}
     </div>
   )
 }
