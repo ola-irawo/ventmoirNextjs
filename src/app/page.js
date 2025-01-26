1a369a210b3f538e3fa4ee285af51269007a29ae
Irawo <timothyaremu63@gmail.com>
2025-01-26 23:40:57 +0100

    remove the console.log

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,6 @@ import styles from "./page.module.css";
 import HomeLayout from "../features/pages/home/layout/HomeLayout";
 
 export default function Home() {
-  console.log("home is");
   return (
     <div className={styles.page}>
       <HomeLayout />
